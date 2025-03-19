ec8df1f07fc8a10b609afd81aa0a28447b015256
FoxKnight679 <felipewerncke17@gmail.com>
2025-03-19 17:24:43 -0300

    update

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Gallery, {Profile, Favorites, Image} from './components/Gallery';
 import { Activitie } from './components/Activitie';
 import { Home } from './screens/home';
 import { Login } from './screens/login';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({  //Usado para criar o 100% do espaço da página
-  container:{
+  container:{ 
       flex: 1,
   },
 })
